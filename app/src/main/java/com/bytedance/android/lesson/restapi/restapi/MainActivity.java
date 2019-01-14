f7c194a0509efe05b8991029010e2a137535d91f
jiangyuzhong <iyuzhong@foxmail.com>
2019-01-14 21:36:48 +0800

    feat: example of parsing json with Gson

--- app/src/main/java/com/bytedance/android/lesson/restapi/restapi/MainActivity.java
+++ app/src/main/java/com/bytedance/android/lesson/restapi/restapi/MainActivity.java
@@ -4,31 +4,40 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.bytedance.android.lesson.restapi.restapi.bean.AndroidOS;
+import com.bytedance.android.lesson.restapi.restapi.bean.OSList;
+import com.google.gson.Gson;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static String RAW = "{\"os\":[{\"name\":\"Pie\",\"code\":28},{\"name\":\"Oreo\",\"code\":27}]}";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView tv = findViewById(R.id.tv);
-        tv.setText(parseFirstName());
+        tv.setText(parseFirstNameWithGson());
     }
 
-    private static String parseFirstName() {
+    private static String parseFirstNameWithGson() {
+        OSList list = new Gson().fromJson(RAW, OSList.class);
+        return list.getOs()[0].getName();
+    }
+
+    private static String parseFirstNameWithJSON() {
         String result = null;
         try {
-            String raw = "{\"os\":[{\"name\":\"Pie\",\"code\":28},{\"name\":\"Oreo\",\"code\":27}]}";
-            JSONObject root = new JSONObject(raw);
+            JSONObject root = new JSONObject(RAW);
             JSONArray os = root.optJSONArray("os");
             result = os.optJSONObject(0).optString("name");
         } catch (JSONException e) {
             e.printStackTrace();
         }
         return result;
-
     }
 }
