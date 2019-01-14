a855b242bae0733c6f9dece9fca235d302dbbc45
jiangyuzhong <iyuzhong@foxmail.com>
2019-01-14 21:26:22 +0800

    feat: example of parsing json with org.json.JSONObject

--- app/src/main/java/com/bytedance/android/lesson/restapi/restapi/MainActivity.java
+++ app/src/main/java/com/bytedance/android/lesson/restapi/restapi/MainActivity.java
@@ -2,6 +2,11 @@ package com.bytedance.android.lesson.restapi.restapi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv = findViewById(R.id.tv);
+        tv.setText(parseFirstName());
+    }
+
+    private static String parseFirstName() {
+        String result = null;
+        try {
+            String raw = "{\"os\":[{\"name\":\"Pie\",\"code\":28},{\"name\":\"Oreo\",\"code\":27}]}";
+            JSONObject root = new JSONObject(raw);
+            JSONArray os = root.optJSONArray("os");
+            result = os.optJSONObject(0).optString("name");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return result;
+
     }
 }
